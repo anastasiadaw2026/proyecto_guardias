946bc9f0a5ada32de37249404669c3a5de47a8d5
Anastasia <anastasia.daw.2026@gmail,com>
2026-05-02 16:34:44 +0200

    AÑADIR LA OPCIÓN DE VACIAR Y SACAR EL INIT

--- bbdd/carga_inicial.py
+++ bbdd/carga_inicial.py
@@ -4,58 +4,47 @@ from lib.profesor import Profesor
 
 
 class CargaInicial:
-    # a lo mejor se puede poner un init con cursor y conexión
+    def __init__(self): # a lo mejor no necesito la conexión
+        self._conexion, self._cursor = self._abrir_conexion()
+
     def _vaciar_bbdd(self):
-        conexion, cursor = self._abrir_conexion()
         vaciar = ('TRUNCATE TABLE PROFESORES')
-        cursor.execute(vaciar)
-        # self._cerrar_conexion(cursor, conexion)
+        self._cursor.execute(vaciar)
 
     def _abrir_conexion(self):
         # todo: ponerse el try
         conexion = mysql.connector.connect(user='root', password='',
-                                           host='127.0.0.1',
-                                           database='gestion_guardias')
+                      host='127.0.0.1', database='gestion_guardias')
         conexion.autocommit = True
         cursor = conexion.cursor()
-        # except mysql.connector.Error as err:
-        #     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        #         print("Error de conexión.")
-        #     elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        #         print("No existe dicha base de datos.")
-        #     else:
-        #         print(err)  # esto me saca el error como es
         return conexion, cursor
 
     def _generar_profesor(self):
         datos_profesor: list = []
         try:
-            with (open('fichero_delphos', encoding = 'utf-8',
-                       mode = 'r') as f):
+            with (open('fichero_delphos', 'r', encoding = 'utf-8') as f):
                 for index, line in enumerate(f.readlines()):
                     if index > 0:
                         datos_profesor = line.strip('"\n').split(',')
                         profesor = Profesor(datos_profesor[0].strip(),
                                             datos_profesor[1].strip())
-
                         self._cargar_profesores_bbdd(profesor)
-        except OSError: # todo desarrollar las excepciones
+        except OSError: # todo: desarrollar las excepciones
             print(f'No se pudo abrir el fichero.')
         except:
             print('error')
 
     def _cargar_profesores_bbdd(self, profesor: Profesor):
-        conexion, cursor = self._abrir_conexion()
         aniadir_profesor = (f"INSERT INTO PROFESORES VALUES('{profesor.id}',"
                                 f"'{profesor.nombre}','{profesor.apellidos}',"
                                 f"'{profesor.clave_encriptada}')")
-        cursor.execute(aniadir_profesor)
-        # self._cerrar_conexion(cursor, conexion)
+        self._cursor.execute(aniadir_profesor)
 
-    def _cerrar_conexion(self, cursor, conexion):
-        cursor.close()
-        conexion.close()
+    def _cerrar_conexion(self):
+        self._cursor.close()
+        self._conexion.close()
 
-# c = CargaInicial()
-# c._vaciar_bbdd()
-# c._generar_profesor()
+c = CargaInicial()
+c._vaciar_bbdd()
+c._generar_profesor()
+c._cerrar_conexion()
